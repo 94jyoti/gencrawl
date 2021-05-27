4cacaaa04fa4ec002eb2c1ef0d0207c8811379cc
Jyoti Varshney <jyotiagrawal94>
2021-05-27 13:49:13 +0530

    add changes for nuveen and american beacon

--- gencrawl/spiders/financial/detail/americanbeacon_com.py
+++ gencrawl/spiders/financial/detail/americanbeacon_com.py
@@ -9,51 +9,59 @@ import urllib.parse
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 class AmericanbeaconDetail(FinancialDetailSpider):
     name = 'financial_detail_americanbeacon_com'
-    #allowed_domains = ['api.nuveen.com','www.nuveen.com']
-    
+
+    # allowed_domains = ['api.nuveen.com','www.nuveen.com']
+
     def get_items_or_req(self, response, default_item={}):
-        
-        # request = []
+
+        parsed_items = []
         items = self.prepare_items(response, default_item)
-        #return_data=item['temp_class_returns'][0]
-        #print("itemdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",items)
+        # return_data=item['temp_class_returns'][0]
+        # print("itemdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",items)
         for i in items:
-            temp_data_returns=i['class_returns']
-            temp_expense_ratio=i['class_expense_ratios']
-            temp_class_benchmark=i['class_benchmarks']
-            temp_sec_30=i['sec_30']
-            #print(temp_data_returns)
-            count=1
+            temp_data_returns = i['class_returns']
+            temp_expense_ratio = i['class_expense_ratios']
+            temp_class_benchmark = i['class_benchmarks']
+            temp_sec_30 = i['sec_30']
+            temp_fund_manager_list=i['temp_fund_managers']
+            #print(temp_fund_manager_list)
+            count = 1
+            fund_managers_list=[]
+            for fund in temp_fund_manager_list:
+                print(fund)
+                dict= {"fund_manager": "", "fund_manager_years_of_experience_in_industry": "", "fund_manager_firm": "",
+                       "fund_manager_years_of_experience_with_fund": "",
+                       'fund_managers': str(temp_fund_manager_list[fund]).split(",")[0],
+                       'fund_manager_years_of_experience_with_fund': str(temp_fund_manager_list[fund]).split(" ")[-1]}
+                fund_managers_list.append(dict)
+            i['fund_managers']=fund_managers_list
             for data in temp_data_returns:
                 for j in data:
-                    if(i['share_class']== data['share_class_1'].split(" ")[0].strip()):
-                        i['share_inception_date']=data['inception_date']
+                    if (i['share_class'] == data['share_class_1'].split(" ")[0].strip()):
+                        i['share_inception_date'] = data['inception_date']
                         break
                 for expense in temp_expense_ratio:
                     for ex in expense:
-                        if(i['share_class']==expense['expense_ratios_share_class']):
-                            i['total_expense_gross']=expense['expense_ratios_gross']
-                            i['total_expense_net']=expense['expense_ratios_net']
-                bench_mark=[]
+                        if (i['share_class'] == expense['expense_ratios_share_class']):
+                            i['total_expense_gross'] = expense['expense_ratios_gross']
+                            i['total_expense_net'] = expense['expense_ratios_net']
+                bench_mark = []
                 for benchmark in temp_class_benchmark:
                     bench_mark.append(benchmark['benchmarks'])
-                i['benchmarks']=bench_mark
+                i['benchmarks'] = bench_mark
                 for sec in temp_sec_30:
                     for s in sec:
-                        if(i['share_class']==sec['sec_30_share_class']):
-                            i['sec_yield_30_day']=sec['sec_30_actual']
-                            i['sec_yield_without_waivers_30_day']=sec['sec_30_unsubsized']
+                        if (i['share_class'] == sec['sec_30_share_class']):
+                            i['sec_yield_30_day'] = sec['sec_30_actual']
+                            i['sec_yield_without_waivers_30_day'] = sec['sec_30_unsubsized']
+                #i['']
+            parsed_items.append(self.generate_item(i, FinancialDetailItem))
+        return parsed_items
 
-                #del i['sec_30']
-                #del i['class_expense_ratios']
-                #del i['class_benchmarks']
-                #del i['class_returns']
-#        print("doooookdnwdwdnwdwkdwkdkdnwdwdkndkwnwdnwkkn",items[0])
-        yield self.generate_item(items[0], FinancialDetailItem)
-
-        #data=[]
+        # data=[]
         '''
         soup = BeautifulSoup(return_data)
         table = soup.find('table', attrs={'id':'grdPerformance'})
@@ -72,18 +80,18 @@ class AmericanbeaconDetail(FinancialDetailSpider):
         df_return_data=pd.DataFrame(return_data[1:],columns=return_data[0])
         print(df_return_data)
         '''
-        #soup = BeautifulSoup(return_data, 'html.parser')
-        #table = soup.find('table', attrs={'id':'grdPerformance'})
-        #keys =[ele.text.strip() for ele in soup.find_all('th')]
-        #rows = table.find_all('tr')
-        
-        #vals = [i.text for i in soup.find('tr', {'class': None}).find_all('td')]
-        #for row in rows:
-            #for col in row.find_all('td'):
-         #   cols = row.find_all('td')
-          #  cols = [ele.text.strip() for ele in cols]
-           # data.append([ele for ele in cols if ele])
-            #data = [ele for ele in data if ele]
-        #vals=data
-        #my_dict = dict(zip(keys, vals))
-        #print (my_dict)
+        # soup = BeautifulSoup(return_data, 'html.parser')
+        # table = soup.find('table', attrs={'id':'grdPerformance'})
+        # keys =[ele.text.strip() for ele in soup.find_all('th')]
+        # rows = table.find_all('tr')
+
+        # vals = [i.text for i in soup.find('tr', {'class': None}).find_all('td')]
+        # for row in rows:
+        # for col in row.find_all('td'):
+        #   cols = row.find_all('td')
+        #  cols = [ele.text.strip() for ele in cols]
+        # data.append([ele for ele in cols if ele])
+        # data = [ele for ele in data if ele]
+        # vals=data
+        # my_dict = dict(zip(keys, vals))
+        # print (my_dict)
--- gencrawl/spiders/financial/detail/nuveen_com.py
+++ gencrawl/spiders/financial/detail/nuveen_com.py
@@ -17,17 +17,34 @@ class NuveenComDetail(FinancialDetailSpider):
         # request = []
         items = self.prepare_items(response, default_item)
         item=items[0]
-        print("itemdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",item)
-        
+        #print("itemdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",item)
+        temp_class_benchmark=item['temp_benchmarks']
+        print("feffessfhhhhhhhhvjhbiiiiinvvvvvvvvvvvvvvvvvvvvvvvv....v....................................",temp_class_benchmark)
+        '''
+        bench_mark = []
+        for benchmark in range(len(temp_class_benchmark)-1):
+            bench_mark.append(temp_class_benchmark[benchmark])
+        item['benchmarks'] = bench_mark
+        '''
         # for manager in item["fund_managers"]:
             # for key,value in manager.iteritems():
                 # manager[key]=
         cusip_id=item['cusip']
+        item['share_class']=item['fund_url'].split("=")[-1]
+        fund_managers_list=[]
+        
+        for i in item['temp_fund_managers']:
+        	data_dict={"fund_manager": "", "fund_manager_years_of_experience_in_industry": "", "fund_manager_firm": "","fund_manager_years_of_experience_with_fund": ""}
+        	data_dict['fund_manager']=i.strip()
+        	fund_managers_list.append(data_dict)
+        	
+        item['fund_managers']=fund_managers_list
+        #print(item['share_class'])
         meta = response.meta
         meta['items'] = item
         api_url="https://api.nuveen.com/MF/ProductDetail/DistributionHistory/"+cusip_id
         #api_url="https://www.nuveen.com/en-us/mutual-funds"
-        print(api_url)
+        #print(api_url)
         item['api_url']=api_url
         r=self.make_request(api_url, callback=self.parse_performance_response, meta=meta)
         return r
@@ -35,7 +52,7 @@ class NuveenComDetail(FinancialDetailSpider):
         #return response
 
     def parse_performance_response(self, response):
-        print('wdwaaaaaaaaaaaddddddddddddddddddddddddd')
+        #print('wdwaaaaaaaaaaaddddddddddddddddddddddddd')
         items = response.meta['items']
         response_json = json.loads(response.text)
         #print("reso.....................................",response_json)
@@ -44,6 +61,34 @@ class NuveenComDetail(FinancialDetailSpider):
         #items = items[0]
         #except:
          #   print("done")
+        capital_gains_list = []
+        dividend_history=[]
+        for i in response_json:
+            data_dict1 = {"ex_date": "", "pay_date": "", "ordinary_income": "", "qualified_income": "", "record_date": "", "per_share": "", "reinvestment_price": ""}
+            data_dict2 = {'long_term_per_share': "", 'cg_ex_date': "", 'cg_record_date': "", 'cg_pay_date': "",'short_term_per_share': "", 'total_per_share': "", 'cg_reinvestment_price': ""}
+            data_dict1['qualified_income'] = None
+            data_dict1['ex_date'] = i['exdivdt']
+            data_dict1['record_date'] = i['rcrddt']
+            data_dict1['pay_date'] = i['paydt'].split("T")[0]
+            data_dict1['per_share'] = None
+            data_dict1['reinvestment_price'] = None
+            data_dict1['ordinary_income'] = i['ordinaryincome']
+            dividend_history.append(data_dict1)
+            data_dict2['long_term_per_share'] = None
+            data_dict2['cg_ex_date'] = i['exdivdt']
+            data_dict2['cg_record_date'] = i['rcrddt']
+            data_dict2['cg_pay_date'] = i['paydt'].split("T")[0]
+            data_dict2['short_term_per_share'] = None
+            data_dict2['total_per_share'] = None
+            data_dict2['cg_reinvestment_price'] = None
+            capital_gains_list.append(data_dict2)
+        items['capital_gains'] = capital_gains_list
+        items['dividends']=dividend_history
+        
+        
+        
+        '''
+        
         capital_gains_list = []
         for i in response_json:
             data_dict = {'long_term_per_share': "", 'cg_ex_date': "", 'cg_record_date': "", 'cg_pay_date': "",
@@ -59,5 +104,6 @@ class NuveenComDetail(FinancialDetailSpider):
             capital_gains_list.append(data_dict)
  
         items['capital_gains'] = capital_gains_list
-        print("bvbvvvvvvvvvvvvvvvvvvvvvvv",items)
+        '''
+        #print("bvbvvvvvvvvvvvvvvvvvvvvvvv",items)
         yield self.generate_item(items, FinancialDetailItem)
