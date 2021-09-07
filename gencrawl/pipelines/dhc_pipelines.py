c67542116064fecdcf63a403e2e07a15271886a6
Sagar Arora <sagararora19992@gmail.com>
2021-09-07 16:41:29 +0530

    DHC pipelines changes to fetch addresses, state & zip

--- gencrawl/pipelines/dhc_pipelines.py
+++ gencrawl/pipelines/dhc_pipelines.py
@@ -3,13 +3,26 @@ from gencrawl.util.utility import Utility
 from gencrawl.util.statics import Statics
 import csv
 import os
+import re
 from gencrawl.settings import RES_DIR
 
 
 class DHCPipeline:
 
-    def open_spider(self, spider):
+    def __init__(self):
         self.redundant_fields = ['temp_fields']
+        self.pincode_rgx = re.compile(r'([\d]{5})')
+        self.state_rgx = re.compile(r'\s([A-Z]{2})\s')
+        us_states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware',
+                     'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
+                     'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+                     'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico',
+                     'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+                     'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+                     'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming', 'District of Columbia']
+        self.us_states = sorted(us_states, key=len, reverse=True)
+
+    def open_spider(self, spider):
         self.decision_tags = spider.config.get("decision_tags") or {}
 
     def parse_field(self, field):
@@ -41,9 +54,43 @@ class DHCPipeline:
                 item['middle_name'] = raw_name[1]
         return item
 
+    def parse_phone(self, item):
+        if item.get("phone"):
+            item['phone'] = item['phone'].replace("tel:", "")
+        return item
+
+    def parse_address(self, item):
+        if not item.get("address"):
+            address_keys = ['address_line_1', 'city', 'state', 'zip']
+            address_values = [item[k] for k in address_keys if item.get(k)]
+            item['address'] = ' '.join(address_values)
+
+        if item.get('address'):
+            address = item['address']
+            if not item.get('zip'):
+                pincode = self.pincode_rgx.search(address, re.S)
+                if pincode:
+                    item['zip'] = pincode.group(1)
+
+            if not item.get("state"):
+                for state in self.us_states:
+                    if state.lower() in address.lower():
+                        item['state'] = state
+                        break
+
+            if item.get("state"):
+                state = self.state_rgx.search(address.replace(",", ' ').replace(
+                    ';', ' ').replace('\n', ' ').replace('\t', ' '), re.S)
+                if state:
+                    item['state'] = state.group(1)
+
+        return item
+
     def process_item(self, item, spider):
         if isinstance(item, HospitalDetailItem):
             item = self.parse_item(item)
             if not self.decision_tags.get("dont_split_name"):
                 item = self.split_name(item)
+            item = self.parse_phone(item)
+            item = self.parse_address(item)
         return item
