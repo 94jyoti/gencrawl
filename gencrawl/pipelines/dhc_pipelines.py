6245baad69799e8b94b74bc3c61467d65caceb4a
Sagar Arora <sagararora19992@gmail.com>
2021-09-13 14:42:46 +0530

    changes in Dhc pipeline

--- gencrawl/pipelines/dhc_pipelines.py
+++ gencrawl/pipelines/dhc_pipelines.py
@@ -179,16 +179,16 @@ class DHCPipeline:
                         break
 
             if not item.get("state"):
-                for state in self.us_states:
-                    if state.lower() in address.lower():
-                        item['state'] = state
-                        break
-
-            if not item.get("state"):
                 state = self.state_rgx.search(address.replace(",", ' ').replace(
                     ';', ' ').replace('\n', ' ').replace('\t', ' '), re.S)
                 if state:
                     item['state'] = state.group(1)
+
+            if not item.get("state"):
+                for state in self.us_states:
+                    if state.lower() in address.lower():
+                        item['state'] = state
+                        break
 
             if not item.get("phone"):
                 for rgx in self.phone_rgx:
@@ -200,13 +200,12 @@ class DHCPipeline:
             for key in ['zip', 'phone', 'state']:
                 val = item.get(key) or ''
                 address = address.replace(val, '').strip(" ,\n")
-
             if not item.get("city"):
                 city = self.parse_city(address)
-                if not city:
-                    for city in self.us_cities:
-                        if city.lower() in address.lower():
-                            break
+                # if not city:
+                #     for city in self.us_cities:
+                #         if city.lower() in address.lower():
+                #             break
                 item['city'] = city
 
             if not item.get("address_line_1"):
