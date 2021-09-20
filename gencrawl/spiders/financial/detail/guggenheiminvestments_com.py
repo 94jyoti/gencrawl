5d1afb7d9833e5890a48503e87d30a14ed4b3e2c
jyotiagrawal94 <jyoti.varshney@forage.ai>
2021-09-20 10:30:48 +0530

    Update guggenheiminvestments_com.py

--- gencrawl/spiders/financial/detail/guggenheiminvestments_com.py
+++ gencrawl/spiders/financial/detail/guggenheiminvestments_com.py
@@ -18,4 +18,36 @@ class GuggenheimComDetail(FinancialDetailSpider):
             if(items[0]['share_class'].lower().replace("class","").strip() in temp_class):
                 items[0]['cusip']=row.xpath(".//td[3]//text()").extract()[0]
                 items[0]['share_inception_date']=row.xpath(".//td[4]//text()").extract()[0]
-        yield self.generate_item(items[0], FinancialDetailItem)
+        meta = response.meta
+        meta['items'] = items
+        #url="https://www.guggenheiminvestments.com/"+response.xpath("(//a[contains(text(),'Distributions')])[2]//@href").extract()[0]
+        url=items[0]['fund_url']+"/distributions"
+        yield self.make_request(url, callback=self.dividends, meta=meta, dont_filter=True,method=Statics.CRAWL_METHOD_SELENIUM)
+
+    def dividends(self, response):
+        items = response.meta['items']
+        file=open("guggen.html","w")
+        file.write(response.text)
+        file.close()
+        excel_url="https://www.guggenheiminvestments.com"+response.xpath("//a[contains(text(),'Export to Excel')]//@href").extract()[0]
+        meta = response.meta
+        meta['items'] = items
+        '''
+        with open('websites.csv', 'rbU') as csv_file:
+            data = csv.reader(csv_file)
+            scrapurls = []
+            for row in data:
+                scrapurls.append(row)
+            return scrapurls
+        '''
+
+        yield self.make_request(excel_url, callback=self.parse_distributions, meta=meta, dont_filter=True,method=Statics.CRAWL_METHOD_SELENIUM)
+
+    def parse_distributions(self,response):
+        items = response.meta['items']
+        print(items)
+        file = open("guggen1.html", "w")
+        file.write(response.text)
+        file.close()
+
+        #yield self.generate_item(items[0], FinancialDetailItem)
