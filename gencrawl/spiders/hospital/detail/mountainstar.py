cb1d43305172f348a22667e6910d653e2e5b95d7
jyotiagrawal94 <jyoti.varshney@forage.ai>
2021-12-03 22:54:01 +0530

    all code

--- gencrawl/spiders/hospital/detail/mountainstar.py
+++ gencrawl/spiders/hospital/detail/mountainstar.py
@@ -21,6 +21,7 @@ class MountainStarhospitalDetail(HospitalDetailSpider):
         file.close()
         data = re.findall("var physician = (.*?);",response.text)[0]
         json_data = json.loads(data)
+        #print(json_data)
         item['first_name'] = json_data['physicianFirstName']
         item['middle_name'] = json_data['physicianMiddleInitial']
         item['last_name'] = json_data['physicianLastName']
@@ -37,6 +38,30 @@ class MountainStarhospitalDetail(HospitalDetailSpider):
         for aff in json_data['affiliations']:
             provider_affiliation.append(aff['locationName'])
         item['affiliation'] = ",".join(provider_affiliation)
+        temp_item=[]
+
+        for count,ele in enumerate(json_data['providerLocations']):
+            print(ele)
+            print(count)
+            temp_item.append(deepcopy(item))
+            temp_item[count]['zip']=json_data['providerLocations'][count]['zip']
+            temp_item[count]['city']=json_data['providerLocations'][count]['city']
+            temp_item[count]['state']=json_data['providerLocations'][count]['state']
+            temp_item[count]['phone']=json_data['providerLocations'][count]['phone']
+            temp_item[count]['fax']=json_data['providerLocations'][count]['fax']
+            temp_item[count]['practice_name']=json_data['providerLocations'][count]['name']
+            temp_item[count]['address_line_1']=json_data['providerLocations'][count]['street']
+            if (temp_item[count]['address_line_1'] == temp_item[count]['city']):
+                temp_item[count]['city'] = ""
+            temp_item[count]['address_raw'] = temp_item[count]['practice_name'] + " " + temp_item[count]['address_line_1'] + " " + temp_item[count]['city'] + " " + \
+                                  temp_item[count]['state'] + " " + temp_item[count]['zip']
+            yield self.generate_item(temp_item[count], HospitalDetailItem)
+
+
+
+
+
+'''
         item['zip']=json_data['displayLocation']['zip']
         item['city'] = json_data['displayLocation']['city']
         item['state'] = json_data['displayLocation']['state']
@@ -47,4 +72,5 @@ class MountainStarhospitalDetail(HospitalDetailSpider):
         if(item['address_line_1']==item['city']):
             item['city']=""
         item['address_raw'] = item['practice_name']+" "+item['address_line_1'] +" " +item['city']+ " "+item['state']+" "+ item['zip']
-        yield self.generate_item(item, HospitalDetailItem)
+        #yield self.generate_item(item, HospitalDetailItem)
+'''
