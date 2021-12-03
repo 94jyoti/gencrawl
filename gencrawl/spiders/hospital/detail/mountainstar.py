c32e803dc42a9cbf6bfaf6ff35a55ae92d10a20c
jyotiagrawal94 <jyoti.varshney@forage.ai>
2021-12-03 13:35:57 +0530

    code

--- gencrawl/spiders/hospital/detail/mountainstar.py
+++ gencrawl/spiders/hospital/detail/mountainstar.py
@@ -44,5 +44,7 @@ class MountainStarhospitalDetail(HospitalDetailSpider):
         item['fax'] = json_data['displayLocation']['fax']
         item['practice_name'] = json_data['displayLocation']['name']
         item['address_line_1'] = json_data['displayLocation']['street']
+        if(item['address_line_1']==item['city']):
+            item['city']=""
         item['address_raw'] = item['practice_name']+" "+item['address_line_1'] +" " +item['city']+ " "+item['state']+" "+ item['zip']
         yield self.generate_item(item, HospitalDetailItem)
