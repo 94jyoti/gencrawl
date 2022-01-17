a375b57831f19f297b5a3e6f2233d6faed5b54a2
Sagar Arora <sagararora19992@gmail.com>
2022-01-17 14:11:16 +0530

    field as item changes

--- gencrawl/spiders/hospital/hospital_detail_field_as_item.py
+++ gencrawl/spiders/hospital/hospital_detail_field_as_item.py
@@ -11,17 +11,25 @@ class HospitalDetailPhoneAsAddressSpider(HospitalDetailSpider):
     name = f'{crawl_domain}_{Statics.CRAWL_TYPE_DETAIL}_field_as_item'
     address_fields = ['practice_name', 'address_raw', 'address', 'address_line_1', 'address_line_2', 'address_line_3',
                       'city', 'state', 'zip', 'phone', 'fax']
-
     # will create a new item for each phones found in phone_as_address
     def get_field_as_item(self, items):
         item = items[0]
         field_map = {"phone_as_item": "phone", "practice_as_item": "practice_name", "fax_as_item": "fax"}
+        addresses_raw = []
+        for item in items:
+            addr_raw = item.get("address_raw")
+            if addr_raw:
+                addresses_raw.append(addr_raw)
+        addresses_raw = '___'.join(addresses_raw)
+
         for field in field_map:
             field_values = item.get(field)
             if field_values:
                 if isinstance(field_values, str):
                     field_values = [field_values]
                 for val in field_values:
+                    if val and val.strip() in addresses_raw:
+                        continue
                     item_replica = deepcopy(item)
                     for key in self.address_fields:
                         item_replica[key] = None
