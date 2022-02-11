ca8567acfbefdc491c0e014816402e4172559f8d
shivam-100 <shivam.dixit@forage.ai>
2022-02-11 15:36:25 +0530

    address and phone issue fixed

--- gencrawl/spiders/hospital/detail/gearycommunityhospital_org.py
+++ gencrawl/spiders/hospital/detail/gearycommunityhospital_org.py
@@ -13,19 +13,27 @@ class GearycommunityhospitalOrgHospitalDetail(HospitalDetailSpider):
         temp_address = list()
         raw_address = response.xpath('//div[@class="wpb_wrapper"]/h3/following-sibling::p').getall()
         extra_address = response.xpath('//p[contains(.,"This provider practices at:")]/following-sibling::p').getall()
+        extra_address_2 = response.xpath('//div[@class="wpb_wrapper"]//p').get()
 
         if raw_address:
             raw_address = re.split(r'(\d{5})', ''.join(raw_address))
             raw_address = ''.join(raw_address[:2])
             temp_address.append(raw_address)
+
         if extra_address:
             extra_address = re.split(r'(\d{5})', ''.join(extra_address))
             extra_address = ''.join(extra_address[:2])
             temp_address.append(extra_address)
 
+        print('--------', temp_address)
+        print('++++++++', extra_address_2)
+
         if temp_address:
             for address in temp_address:
                 items['address_raw'] = address
                 yield self.generate_item(items, HospitalDetailItem)
+        elif extra_address_2:
+            items['address_raw'] = extra_address_2
+            yield self.generate_item(items, HospitalDetailItem)
         else:
             yield self.generate_item(items, HospitalDetailItem)
