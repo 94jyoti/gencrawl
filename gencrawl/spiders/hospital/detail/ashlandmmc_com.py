ecf91a4c135974ea97ccb670dbb960298f9ba5b5
shivam-100 <shivam.dixit@forage.ai>
2022-02-14 18:26:30 +0530

    fixed the address issue

--- gencrawl/spiders/hospital/detail/ashlandmmc_com.py
+++ gencrawl/spiders/hospital/detail/ashlandmmc_com.py
@@ -12,13 +12,16 @@ class AshlandmmcComHospitalDetail(HospitalDetailSpider):
         items = deepcopy(items[0])
         raw_address = response.xpath('(//div[contains(@style,"padding:")])[last()]').get()
 
-        raw_address_1 = ''.join(re.findall(r'Location:([\s\S]+\d{3}-\d{3}-\d{4})', raw_address))
-        raw_address_2 = ''.join(re.findall(r'Clinic:([\s\S]+\d{3}-\d{3}-\d{4} | [\s\S]+\(\d{3}\)\s\d{3}-\d{4})',
-                                           raw_address))
-        raw_address_3 = ''.join(re.findall
+        raw_address_1 = ''.join(re.findall(r'Loc.*:([\s\S]+\d{3}-\d{3}-\d{4})', raw_address))
+        raw_address_2 = ''.join(re.findall(r'Loc.*:([\s\S]+\(\d{3}\)\s\d{3}-\d{4})', raw_address))
+        raw_address_3 = ''.join(re.findall(r'Loc.*:([\s\S]+)', raw_address))
+        raw_address_4 = ''.join(re.findall(r'Loc.*:([\s\S]+\(\d{3}\)\s\d{3}-\d{4})', raw_address))
+        raw_address_5 = ''.join(re.findall(r'Cli.*:([\s\S]+\d{3}-\d{3}-\d{4})', raw_address))
+        raw_address_6 = ''.join(re.findall(r'Cli.*:([\s\S]+\(\d{3}\)\s\d{3}-\d{4})', raw_address))
+        raw_address_7 = ''.join(re.findall
                                 (r'<p> <\/p>\n<p> <\/p>\n<p> <\/p>\n<p> <\/p>\n<p> <\/p>\n<p> <\/p>'
                                  r'([\s\S]+)', raw_address))
-        raw_address_4 = ''.join(re.findall(r'<h2><\/h2>\n<h2><\/h2>([\s\S]+)', raw_address))
+        raw_address_8 = ''.join(re.findall(r'<h2><\/h2>\n<h2><\/h2>([\s\S]+)', raw_address))
 
         speciality = ''.join(items['speciality'])
 
@@ -28,7 +31,15 @@ class AshlandmmcComHospitalDetail(HospitalDetailSpider):
             items['address_raw'] = raw_address_2.replace(speciality, '')
         elif raw_address_3:
             items['address_raw'] = raw_address_3.replace(speciality, '')
-        else:
+        elif raw_address_4:
             items['address_raw'] = raw_address_4.replace(speciality, '')
+        elif raw_address_5:
+            items['address_raw'] = raw_address_5.replace(speciality, '')
+        elif raw_address_6:
+            items['address_raw'] = raw_address_6.replace(speciality, '')
+        elif raw_address_7:
+            items['address_raw'] = raw_address_7.replace(speciality, '')
+        else:
+            items['address_raw'] = raw_address_8.replace(speciality, '')
 
         yield self.generate_item(items, HospitalDetailItem)
